1d1fe13be9e84c23f0120f9d7a2761c457ffbc17
Geert van der Ploeg <gvanderploeg@iprofs.nl>
2014-06-18 15:40:44 +0200

    tuned frontend iframes, fixed some settings

--- playlists/urls.py
+++ playlists/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('playlists.views',
-                       url(r'^$', 'index'),
                        url(r'^(?P<device_id>\d+)/$', 'detail'),
                        )
