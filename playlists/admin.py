e845d50f24d5a29a037c1bcedc46ddff982fff83
Geert van der Ploeg <gvanderploeg@iprofs.nl>
2014-06-19 12:44:47 +0200

    added uuid for future reference in urls instead of ids

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,3 +1,4 @@
+import uuid
 from models import Playlist, Device, Url
 from django.contrib import admin
 
@@ -5,9 +6,11 @@ class UrlInline(admin.StackedInline):
     model = Url
     extra = 3
 
+class DeviceAdmin(admin.ModelAdmin):
+    readonly_fields = ['uuid']
 
 class PlaylistAdmin(admin.ModelAdmin):
     inlines = [UrlInline]
 
 admin.site.register(Playlist, PlaylistAdmin)
-admin.site.register(Device)
+admin.site.register(Device, DeviceAdmin)
--- playlists/models.py
+++ playlists/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from uuid import uuid1
 
 
 class Playlist(models.Model):
@@ -9,6 +10,7 @@ class Playlist(models.Model):
     def __unicode__(self):
         return self.title
 
+
 class Url(models.Model):
     playlist = models.ForeignKey(Playlist)
     url = models.CharField(max_length=255)
@@ -16,8 +18,10 @@ class Url(models.Model):
     def __unicode__(self):
         return self.url
 
+
 class Device(models.Model):
     name = models.CharField(max_length=200)
+    uuid = models.CharField(max_length=200, default=uuid1(), null=True, blank=True) # nullable and blankable to be removed, once all content adhers
     user = models.ForeignKey(User)
     playlist = models.ForeignKey(Playlist)
     interval = models.BigIntegerField('interval in ms', default=10000)
